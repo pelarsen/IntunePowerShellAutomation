0aa96b2b693d7cb4dee8f0f515a17b3d0fc667e4
Per Larsen [MVP] <34544411+PerLarsen1975@users.noreply.github.com>
2018-05-21 09:35:23 +0200

    Add files via upload

---
+++ Edge crash WDAC fix.ps1
@@ -0,0 +1,15 @@
+﻿New-Item -Path HKCU:\Software\Microsoft\Internet Explorer\Spartan –Force 
+$registryPath = "HKCU:\Software\Microsoft\Internet Explorer\Spartan"
+
+#Fix Edge on 1803 with WDAG or Applocker enabled
+$Name = "RAC_LaunchFlags"
+$value = "00000035"
+IF(!(Test-Path $registryPath))
+  {
+    New-Item -Path $registryPath -Force | Out-Null
+    New-ItemProperty -Path $registryPath -Name $name -Value $value `
+    -PropertyType String -Force | Out-Null}
+ ELSE {
+    New-ItemProperty -Path $registryPath -Name $name -Value $value `
+    -PropertyType String -Force | Out-Null}
+
